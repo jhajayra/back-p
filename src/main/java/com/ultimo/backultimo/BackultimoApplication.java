ec11b99f69ca3056561092ef28039ed9a0cf7d09
jhajayra <jhajayra95@gmail.com>
2023-02-21 21:38:01 -0300

    deploy vacio

---
+++ src/main/java/com/ultimo/backultimo/BackultimoApplication.java
@@ -0,0 +1,13 @@
+package com.ultimo.backultimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackultimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackultimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultimo/backultimo/BackultimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultimo.backultimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackultimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
